fc72a8601a75a879d533c8bf0cc7018685f18495
Kilian <76685269+KilianSen@users.noreply.github.com>
2024-08-24 01:42:42 +0200

    added more functionality to the mockup.

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@ import './App.css'
 import {ReactFlow} from "@xyflow/react";
 import '@xyflow/react/dist/style.css';
 import {
-    Button,
+    Badge,
+    Button, ButtonGroup,
     Card,
     CardBody,
-    CardHeader, Chip,
+    CardHeader, Checkbox, Chip, Divider,
     Dropdown,
     DropdownItem,
     DropdownMenu, DropdownSection,
-    DropdownTrigger, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Tooltip
+    DropdownTrigger, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, Tooltip
 } from "@nextui-org/react";
-import {Moon, Save, Signal, Sun} from "lucide-react";
+import {Eye, EyeOff, Github, Moon, Save, Signal, Sun} from "lucide-react";
 import {useTheme} from "next-themes";
 import {ReactElement, useState} from "react";
+import { motion, AnimatePresence } from 'framer-motion';
 
 const initialNodes = [
     { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
@@ -127,36 +129,126 @@ function ChatWindow() {
                 123
             </CardBody>
         </Card>
-        <Modal isDismissable={false} defaultOpen backdrop={"blur"} hideCloseButton size={"3xl"}>
-            <ModalContent>
-                <ModalHeader>
-                    <span>Welcome to OpenConversation</span>
-                    <Card className={"bg-success text-xs px-1 py-0.5 shrink w-min h-min translate-y-1/2 opacity-75"}>v1.1</Card>
-                </ModalHeader>
-                <ModalBody>
-                    OpenConversation is a tool to simulate conversations with multiple AI agents.
-                    <Chip variant={"flat"} color={"warning"} className={"w-full h-fit text-wrap flex-wrap p-2 rounded-xl border-1 border-warning/[.125]"} aria-multiline>This tool is not intended to be used for 1 on 1 conversations, with a human and AI. For that, we recommend using OpenWebUI in conjunction with Ollama!</Chip>
-
-                    <span>Connect to a <Tooltip content={<p>We recommend self-hosting <a className={"text-secondary"} href={"https://ollama.ai"}>Ollama</a>!</p>}>OpenAI API Endpoint</Tooltip></span>
-                </ModalBody>
-
-                <ModalFooter>
-                    <Button>
-                        Lets Go!
-                    </Button>
-                </ModalFooter>
-            </ModalContent>
-        </Modal>
     </div>
+}
+
+function MainMenuModal() {
+
+    function DefaultPage() {
+        return <>
+            <div className={"flex justify-between"}>
+                <div className={"flex flex-col gap-2 max-w-[50%] grow"}>
+                    <h1 className={"text-xl text-default"}>Main</h1>
+                    <Button size={"sm"} className={"justify-start"}>Create New Project</Button>
+                    <Button size={"sm"} className={"justify-start"}>Open Existing Project</Button>
+                    <Button size={"sm"} className={"justify-start"}>Disconnect from API</Button>
+                </div>
+                <div className={"flex justify-start items-end flex-col h-72"}>
+                <h1 className={"text-xl text-default"}>Recent Projects</h1>
+                    <div className={"flex flex-col items-end"}>
+                        <Button variant={"flat"} className={"h-fit"}>
+                            <div className={"flex flex-col items-start py-1"}>
+                                <div>Project 1</div>
+                                <div>Created: 2021-10-10</div>
+                            </div>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        </>
+    }
+
+    function ConnectPage({onClicked}: {onClicked?: () => void}) {
+        const [isAnonymous, setIsAnonymous] = useState(true)
+        const [keyVisible, setKeyVisible] = useState(false)
+
+        return <>
+            <div className={"flex flex-col items-center gap-2"}>
+                <h1>Connect to an OpenAI API</h1>
+                <Input placeholder={"API Endpoint"} type={"password"} className={"w-full"} endContent={<Signal/>} />
+                <Input placeholder={"API Key"} type={!keyVisible?"password":"text"} className={"w-full"} endContent={keyVisible?<Eye onClick={()=>setKeyVisible(!keyVisible)}/>:<EyeOff onClick={()=>setKeyVisible(!keyVisible)}/>} isDisabled={isAnonymous}/>
+                <div className={"flex w-full"}>
+                    <div className={"flex"}>
+                        <Checkbox defaultSelected={isAnonymous} isSelected={isAnonymous} onValueChange={(s) => {
+                            setIsAnonymous(s)
+                            setKeyVisible(false)
+                        }}>Anonymous</Checkbox>
+                    </div>
+                    <div className={"w-full flex justify-end"}>
+                        <Button onClick={onClicked}>Connect</Button>
+                    </div>
+                </div>
+            </div>
+        </>
+    }
+
+    const [page, setPage] = useState(0)
+
+    const animProps = {
+        initial: {opacity: 0, x: 100},
+        animate: {opacity: 1, x: 0},
+        exit: {opacity: 0, x: -100},
+        transition: {duration: .5}
+    }
+
+    return (
+        <>
+            <Modal isDismissable={false} defaultOpen backdrop={"blur"} hideCloseButton size={"3xl"}>
+                <ModalContent>
+                    <ModalHeader>
+                        <div>
+                            <h1 className={"text-2xl"}>Welcome to OpenConversation</h1>
+                            <div className={"flex gap-2"}>
+                                <Chip variant={"flat"} color={"success"} size={"sm"}>v1.0.0</Chip>
+                                <Chip variant={"flat"} color={"default"} size={"sm"} className={"text-foreground/[.5]"}>GitHub</Chip>
+                            </div>
+                        </div>
+                    </ModalHeader>
+                    <ModalBody>
+                        <AnimatePresence mode={"wait"}>
+                            {page == 1 && <motion.div {...animProps} key={0}><DefaultPage/></motion.div>}
+                            {page == 0 && <motion.div {...animProps} key={1}><ConnectPage onClicked={() => setPage(1)}/>
+                            </motion.div>}
+                        </AnimatePresence>
+                    </ModalBody>
+                    <ModalFooter>
+                        <div className={"flex flex-col gap-2"}>
+                            <Chip variant={"flat"} color={"default"}
+                                  className={"min-w-full max-w-full p-2 py-4 rounded-xl border-2 border-default/[.5] text-foreground/[.75]"}>
+                            OpenConversation is a tool to simulate conversations with multiple AI agents.
+                            </Chip>
+                            <Chip variant={"flat"} color={"warning"}
+                                          className={"w-full h-fit text-wrap flex-wrap p-2 rounded-xl border-2 border-warning/[.125] max-w-full"}
+                                          aria-multiline>This tool is not intended to be used for 1 on 1 conversations, with a
+                                        human
+                                        and AI. For that, we recommend using OpenWebUI in conjunction with Ollama!</Chip>
+                            {page == 0 && <Chip variant={"flat"} color={"secondary"}
+                                                className={"w-full h-fit text-wrap flex-wrap p-2 rounded-xl border-2 border-secondary/[.125] text-secondary max-w-full"}
+                                                aria-multiline>For an OpenAI API compatible server we recommend: Ollama</Chip>}
+                            <div className={"w-full flex gap-2 justify-center pt-3"}>
+                                {Array(2).fill(0).map((_, i) => <>
+                                    <div key={i} className={"!aspect-square w-2 bg-default rounded-full" + (i == page ? " bg-foreground/[.3]": "")}/>
+                                </>)}
+                            </div>
+                        </div>
+                    </ModalFooter>
+                </ModalContent>
+                <Chip variant={"faded"} className={"absolute bottom-0 right-0 text-xs z-[999] m-1 p-0"}>
+                    Made in 🇩🇪 🇪🇺 mit ❤️
+                </Chip>
+            </Modal>
+        </>
+    )
 }
 
 
 export default function App() {
     return (
-        <div style={{ width: '100vw', height: '100vh' }}>
-            <MenuBar />
-            <ChatWindow />
+        <div style={{width: '100vw', height: '100vh'}}>
+        <MenuBar/>
+            <ChatWindow/>
             <ReactFlow nodes={initialNodes} edges={initialEdges} proOptions={{hideAttribution: true}}/>
+            <MainMenuModal />
         </div>
     );
 }
